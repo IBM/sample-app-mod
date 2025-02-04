6b5b8ea9b9527c4e7ff24e9115b2b41c16fb99f9
Parvathi Nair <parvathinair@mac.mul.ie.ibm.com>
2025-02-04 15:54:47 +0000

    Code formatting improvements
    Signed-off-by: Parvathi Nair <parvathinair@mac.mul.ie.ibm.com>

--- src/main/java/com/acme/modres/security/SSLUtils.java
+++ src/main/java/com/acme/modres/security/SSLUtils.java
@@ -6,16 +6,16 @@ package com.acme.modres.security;
 import java.util.logging.Logger;
 
 public class SSLUtils {
-    private static final Logger logger = Logger.getLogger(SSLUtils.class.getName());
-    // private SSLContext getContext() throws Exception {
-    //     try {
-    //         SSLContext sc = SSLContext.getInstance("SSL");
-    //         sc.init(null, // we don't need KeyManager
-    //             new TrustManager[]{new FakeX509TrustManager()},
-    //             new java.security.SecureRandom());
-    //         return sc;
-    //     } catch (Exception exc) {
-    //         throw new Exception("Some error");
-    //     }
-    // }
+  private static final Logger logger = Logger.getLogger(SSLUtils.class.getName());
+  // private SSLContext getContext() throws Exception {
+  // try {
+  // SSLContext sc = SSLContext.getInstance("SSL");
+  // sc.init(null, // we don't need KeyManager
+  // new TrustManager[]{new FakeX509TrustManager()},
+  // new java.security.SecureRandom());
+  // return sc;
+  // } catch (Exception exc) {
+  // throw new Exception("Some error");
+  // }
+  // }
 }
--- src/main/java/com/acme/modres/util/JsonInputStream.java
+++ src/main/java/com/acme/modres/util/JsonInputStream.java
@@ -10,42 +10,42 @@ import java.io.InputStreamReader;
 import com.google.gson.Gson;
 
 public class JsonInputStream extends FileInputStream {
-    
-    private File file;
 
-    public JsonInputStream(File file) throws FileNotFoundException {
-        super(file);
-        this.file = file;
-    }
+  private File file;
 
-    public Object parseJsonAs(Class<?> cls) {
-        if (file.exists()){
-            JsonInputStream is = null;
-            Object jsonObject = null;
-            try {
-                is = new JsonInputStream(file);
-                Gson gson = new Gson();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                jsonObject = gson.fromJson(reader, cls); 
-            } catch (Exception e) {
-                e.printStackTrace();
-            } catch (Throwable e) {
-                e.printStackTrace();
-            } finally {
-                if (is != null) {
-                    try {
-                        is.close();
-                        is.read(); // test if file is closed
-                    } catch (IOException e) {
-                        // closed successfully
-                        return jsonObject; 
-                    } catch (Throwable e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+  public JsonInputStream(File file) throws FileNotFoundException {
+    super(file);
+    this.file = file;
+  }
+
+  public Object parseJsonAs(Class<?> cls) {
+    if (file.exists()) {
+      JsonInputStream is = null;
+      Object jsonObject = null;
+      try {
+        is = new JsonInputStream(file);
+        Gson gson = new Gson();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        jsonObject = gson.fromJson(reader, cls);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } catch (Throwable e) {
+        e.printStackTrace();
+      } finally {
+        if (is != null) {
+          try {
+            is.close();
+            is.read(); // test if file is closed
+          } catch (IOException e) {
+            // closed successfully
+            return jsonObject;
+          } catch (Throwable e) {
+            e.printStackTrace();
+          }
         }
-		return null;
-	}
-    
+      }
+    }
+    return null;
+  }
+
 }
