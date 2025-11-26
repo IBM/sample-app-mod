da75fa622e8015352b385d1a78b441dfa8d572ec
Kieran Geoghegan <kieran.geoghegan@ie.ibm.com>
2025-11-26 17:08:59 +0000

    sun misc unsafe

---
+++ src/main/java/com/acme/modres/util/ModResortsSecretData.java
@@ -0,0 +1,21 @@
+package com.acme.modres.util;
+
+/**
+ * Sample class with a private field that will be accessed using Unsafe.
+ * This represents sensitive data in the ModResorts application.
+ */
+public class ModResortsSecretData {
+    
+    @SuppressWarnings("unused")
+    private String secretApiKey = "mod-resorts-secret-key-12345";
+    
+    private int maxReservationLimit = 100;
+    
+    public ModResortsSecretData() {
+    }
+    
+    public String getPublicInfo() {
+        return "This is public information";
+    }
+}
+
